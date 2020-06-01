d84ab1675d96aa8661ba505e0bf3e988cec9bf6e
negar <nbagheri@sfu.ca>
2020-05-31 17:35:25 -0700

    Added the google API_key

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(
       MaterialApp(
         home: Map(),
       ),
     );
+//
+//class Map extends StatefulWidget {
+//  @override
+//  _MapState createState() => _MapState();
+//}
+//
+//class _MapState extends State<Map> {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      backgroundColor: Colors.red.shade100,
+//      appBar: AppBar(
+//        backgroundColor: Colors.red.shade300,
+//        title: Text('SFU Interactive Map'),
+//        centerTitle: true,
+//      ),
+//    );
+//  }
+//}
 
 class Map extends StatefulWidget {
-  @override
-  _MapState createState() => _MapState();
+   @override
+    _MapState createState() => _MapState();
 }
-
+//
 class _MapState extends State<Map> {
   @override
   Widget build(BuildContext context) {
@@ -24,3 +46,5 @@ class _MapState extends State<Map> {
     );
   }
 }
+
+
