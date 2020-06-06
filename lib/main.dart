d0847ae0b0a556f50c700cdb2a86881be6f29c30
rdjauhari <r.djauhari99@gmail.com>
2020-06-05 17:08:11 -0700

    added google maps and search bar

--- lib/main.dart
+++ lib/main.dart
@@ -8,32 +8,22 @@ void main() => runApp(
         home: Map(),
       ),
     );
-//
-//class Map extends StatefulWidget {
-//  @override
-//  _MapState createState() => _MapState();
-//}
-//
-//class _MapState extends State<Map> {
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      backgroundColor: Colors.red.shade100,
-//      appBar: AppBar(
-//        backgroundColor: Colors.red.shade300,
-//        title: Text('SFU Interactive Map'),
-//        centerTitle: true,
-//      ),
-//    );
-//  }
-//}
 
 class Map extends StatefulWidget {
-   @override
-    _MapState createState() => _MapState();
+  @override
+  _MapState createState() => _MapState();
 }
-//
+
 class _MapState extends State<Map> {
+  GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(
+      49.279075, -122.919000); //centers around convocation mall approx.
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,8 +33,51 @@ class _MapState extends State<Map> {
         title: Text('SFU Interactive Map'),
         centerTitle: true,
       ),
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 18.0,
+            ),
+          ),
+          Positioned(
+            top: 10,
+            right: 15,
+            left: 15,
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey[300],
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                color: Colors.grey[300],
+              ),
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.menu),
+                    onPressed: () {/*can add stuff to menu button here*/},
+                  ),
+                  Expanded(
+                    child: TextField(
+                      cursorColor: Colors.black,
+                      keyboardType: TextInputType.text,
+                      textInputAction: TextInputAction.go, //figure out what to do with this...
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(horizontal: 15),
+                        hintText: "Search...",
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
-
-
