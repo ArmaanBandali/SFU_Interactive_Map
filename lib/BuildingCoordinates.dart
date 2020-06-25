668a14b8fb50b6247abce6071f9a16a1b1f6bb2e
rdjauhari <r.djauhari99@gmail.com>
2020-06-25 02:10:21 -0700

    added WMC

--- lib/BuildingCoordinates.dart
+++ lib/BuildingCoordinates.dart
@@ -3,19 +3,21 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Buildings{ //Base building info
   String buildingName;
+  String abbreviation;
   List<LatLng> buildingCoords;
-  Buildings(this.buildingName,this.buildingCoords);
+  Buildings(this.buildingName, this.buildingCoords);
+  Buildings.withAbbreviation(this.buildingName, this.abbreviation, this.buildingCoords);
 }
 
 class AllBuildings{
   List<Buildings> allBuildings = [];
 
   AllBuildings(){
-    allBuildings.add(Buildings('sfu', <LatLng>[ //Example for adding polygon coordinates and id through constructor
-      LatLng(49.279075, -122.919000),
-      LatLng(49.279180, -122.919000),
-      LatLng(49.279180, -122.918090),
-      LatLng(49.279075, -122.918090)
+    allBuildings.add(Buildings.withAbbreviation('West Mall Centre', 'WMC', <LatLng>[ //Example for adding polygon coordinates and id through constructor
+      LatLng(49.279700, -122.922640),
+      LatLng(49.280010, -122.922525),
+      LatLng(49.279775, -122.920975),
+      LatLng(49.279465, -122.921075)
       ])
     );//add
   }//addBuilding
