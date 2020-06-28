874753da8b6efba9cd10444f7f2b6972776020ba
rdjauhari <r.djauhari99@gmail.com>
2020-06-28 00:08:26 -0700

    added functionality for washrooms menu option

--- lib/main.dart
+++ lib/main.dart
@@ -54,6 +54,10 @@ class _MapState extends State<Map> {
       ImageConfiguration(devicePixelRatio: 3.2),
       'assets/foodMarker.png',
     );
+    washroomLocationIcon = await BitmapDescriptor.fromAssetImage(
+      ImageConfiguration(devicePixelRatio: 3.2),
+      'assets/washroommarker.png',
+    );
   }
 
   final LatLng _center = const LatLng(
@@ -167,7 +171,7 @@ class _MapState extends State<Map> {
           //in RCB
           Marker(
             markerId: MarkerId('rcb'),
-            position: LatLng(49.280100, -122.916700),
+            position: LatLng(49.279800, -122.916950),
             icon: studyLocationIcon,
             infoWindow: InfoWindow(
               title: "Study Tables with Outlets (Below Images Theatre)",
@@ -238,8 +242,100 @@ class _MapState extends State<Map> {
         ];
         //washroom markers
         washroomMarkers = [
-
+          //in WMC
+          Marker(
+            markerId: MarkerId('washroom1'),
+            position: LatLng(49.279750, -122.922500),
+            icon: washroomLocationIcon,
+            infoWindow: InfoWindow(
+              title: "Women's Washroom (3F)",
+            ),
+          ),
+          Marker(
+            markerId: MarkerId('washroom2'),
+            position: LatLng(49.279740, -122.922400),
+            icon: washroomLocationIcon,
+            infoWindow: InfoWindow(
+              title: "Men's Washroom (2F)",
+            ),
+          ),
+          Marker(
+            markerId: MarkerId('washroom3'),
+            position: LatLng(49.279635, -122.921625),
+            icon: washroomLocationIcon,
+            infoWindow: InfoWindow(
+              title: "Women's Washroom (3F)",
+            ),
+          ),
+          Marker(
+            markerId: MarkerId('washroom4'),
+            position: LatLng(49.2795760, -122.921300),
+            icon: washroomLocationIcon,
+            infoWindow: InfoWindow(
+              title: "Men's Washroom (3F)",
+            ),
+          ),
+          Marker(
+            markerId: MarkerId('washroom5'),
+            position: LatLng(49.2795730, -122.921100),
+            icon: washroomLocationIcon,
+            infoWindow: InfoWindow(
+              title: "Women's Washroom (2F)",
+            ),
+          ),
+          Marker(
+            markerId: MarkerId('washroom6'),
+            position: LatLng(49.2795735, -122.921150),
+            icon: washroomLocationIcon,
+            infoWindow: InfoWindow(
+              title: "Handicapped Washroom (2F)",
+            ),
+          ),
+          //in MBC
+          Marker(
+            markerId: MarkerId('washroom7'),
+            position: LatLng(49.278800, -122.919075),
+            icon: washroomLocationIcon,
+            infoWindow: InfoWindow(
+              title: "Washrooms (M/F) (2F)",
+            ),
+          ),
+          //in the Library
+          Marker(
+            markerId: MarkerId('washroom8'),
+            position: LatLng(49.279480, -122.918300),
+            icon: washroomLocationIcon,
+            infoWindow: InfoWindow(
+              title: "Men's Washroom (Most Floors)",
+            ),
+          ),
+          Marker(
+            markerId: MarkerId('washroom9'),
+            position: LatLng(49.279575, -122.918270),
+            icon: washroomLocationIcon,
+            infoWindow: InfoWindow(
+              title: "Women's Washroom (Most Floors)",
+            ),
+          ),
+          //in RCB
+          Marker(
+            markerId: MarkerId('washroom10'),
+            position: LatLng(49.279725, -122.916825),
+            icon: washroomLocationIcon,
+            infoWindow: InfoWindow(
+              title: "Women's Washroom (1F)",
+            ),
+          ),
+          Marker(
+            markerId: MarkerId('washroom11'),
+            position: LatLng(49.279750, -122.917000),
+            icon: washroomLocationIcon,
+            infoWindow: InfoWindow(
+              title: "Men's Washroom (1F)",
+            ),
+          ),
         ];
+
 
         _markers.addAll(buildingMarkers);
       },
@@ -256,17 +352,17 @@ class _MapState extends State<Map> {
         _markers.removeAll(washroomMarkers);
         _markers.addAll(buildingMarkers);
       }
-      else if(_selectedChoices.title == MenuChoices().choices[2].title) {
-        _markers.removeAll(foodMarkers);
-        _markers.removeAll(studyMarkers);
-        _markers.removeAll(buildingMarkers);
-        _markers.addAll(washroomMarkers);
-      }
       else if(_selectedChoices.title == MenuChoices().choices[1].title) {
         _markers.removeAll(buildingMarkers);
         _markers.removeAll(studyMarkers);
         _markers.removeAll(washroomMarkers);
         _markers.addAll(foodMarkers);
+      }
+      else if(_selectedChoices.title == MenuChoices().choices[2].title) {
+        _markers.removeAll(foodMarkers);
+        _markers.removeAll(studyMarkers);
+        _markers.removeAll(buildingMarkers);
+        _markers.addAll(washroomMarkers);
       }
       else if(_selectedChoices.title == MenuChoices().choices[3].title) {
         _markers.removeAll(buildingMarkers);
